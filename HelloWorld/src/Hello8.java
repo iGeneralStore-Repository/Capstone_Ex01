bbac050b7da41539491bfca28fa92b24a10a8023
Kyunghwan Abel Bae <igeneralstore@gmail.com>
2015-10-08 18:06:34 +0900

    Kyunghwan

---
+++ HelloWorld/src/Hello8.java
@@ -0,0 +1,19 @@
+
+public class Hello8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("8ights");
+		
+		helloKyunghwan();
+		//helloSeoungwon();
+		//helloJunsu();
+		//helloYoungjo();
+	}
+	
+	public static void helloKyunghwan(){
+		System.out.println("Kyunghwan");
+	}
+
+}
